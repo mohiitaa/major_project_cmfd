6531b4726d93fc30562790c3927e0fcf70b671d2
Your Namemohiitaa <chowdhury.mohita@gmail.com>
2019-01-08 16:33:19 +0530

    Add everything as commandline arguments

--- image_resize.py
+++ image_resize.py
@@ -5,8 +5,9 @@ from PIL import Image
 # mywidth = 300
 mywidth=int(sys.argv[1])
 filename  = str(sys.argv[2])
-img = Image.open(filename + '.jpg')
+ext = str(sys.argv[3])
+img = Image.open(filename + '.' +ext)
 wpercent = (mywidth/float(img.size[0]))
 hsize = int((float(img.size[1])*float(wpercent)))
 img = img.resize((mywidth,hsize), PIL.Image.ANTIALIAS)
-img.save(filename+'_resized.jpg')
+img.save(filename+'_resized'+ '.' +ext)
